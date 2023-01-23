9136dac4aef61f615d9725c51de6969cd74b12e1
Lina Petersson <lipe2106@student.miun.se>
2023-01-23 14:30:55 +0100

    Skapat en grund för momentet

---
+++ moment11/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿"use strict"
+
+//Get element from HTML
+let hamburger = document.getElementById("hamburger");
+let menu = document.getElementById("menu");
+
+//Add eventlistener
+hamburger.addEventListener("click", showMenu);
+
+//Function to show menu
+function showMenu() {
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
